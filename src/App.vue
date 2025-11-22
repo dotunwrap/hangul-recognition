e48aa95fac5f08cb89dade3cc790a2071831f75d
Gabby Simpson <gabby@dotunwrap.dev>
2025-11-22 17:13:30 -0500

    chore: Initial page structures

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="font-bold">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
